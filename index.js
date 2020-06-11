095e25d8746093a70dbf2f334d8bf6108e121ead
AnaRativa09 <anarativa09@gmail.com>
2020-06-11 12:56:55 -0500

    Updates

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const paymentPage = document.getElementById('payment'); //pantalla 2
 const buttonBuy = document.getElementById('btnBuy'); //Botón comprar
 const buttonDisabled = document.querySelector('.btnDisabled'); //Botón agotado, solo selecciona el primer elemento de la clase
 
+const form = document.getElementById('formCard'); //Formulario
+const creditCard = document.getElementById('creditCard'); //Tarjeta crédito
+const numberCreditCard = document.getElementById('dataNumber');
+const inputNumberTC = document.getElementById('inputNumberCard');
+const nameCreditCard = document.getElementById('dataName');
+const inputNameTC = document.getElementById('inputName');
+const monthCreditCard = document.getElementById('month');
+const inputMonthTC = document.getElementById('selectMonth');
+const yearCreditCard = document.getElementById('year');
+const inputYearTC = document.getElementById('selectYear');
+
+//Cambio de pantallas
 buttonBuy.addEventListener('click', showPayment);
 
 function showPayment() {
@@ -13,17 +25,78 @@ function showPayment() {
   instrumentsPage.classList.add('hidden');
 }
 
+//Alerta de no stock para botón "Agotado"
 buttonDisabled.addEventListener('click', noStock);
 
 function noStock() {
   alert('Muy pronto lo tendremos en stock');
 }
 
-
-/*const form = document.querySelector('#formCard');
+//Menú desplegable en el select mes
 for (let i = 1; i <= 12; i++) {
-    let opcion =document.creatElement("option");
-    opcion.value = i;
-    opcion.innerText = i;
-    form.selectMonth.appendChild('opcion');
-}*/
+    let optionMonths = document.createElement('option');
+    optionMonths.value = i;
+    optionMonths.textContent = i;
+    form.selectMonth.appendChild(optionMonths);
+}
+
+// Menú desplegable en el select año
+const yearActual = new Date().getFullYear();
+for(let i = yearActual; i <= yearActual + 4; i++){
+	let optionYears = document.createElement('option');
+	optionYears.value = i;
+	optionYears.textContent = i;
+	form.selectYear.appendChild(optionYears);
+}
+
+//Reflejar Nombre en TC (OPCIÓN 1 PARA LLAMAR AL ID SIN TENER QUE GENERAR CONST)
+form.inputName.addEventListener('keyup', reflectName);
+
+function reflectName(){
+nameCreditCard.textContent = form.inputName.value;
+}
+
+form.inputName.addEventListener('blur', returnReflectName);
+
+function returnReflectName(){
+  if (form.inputName.value == ''){
+  nameCreditCard.textContent = 'Nombre titular';
+  }
+};
+
+/*/ Reflejar Número en TC (OPCIÓN 2 PARA LLAMAR ID DESDE CONST)
+inputNumberTC.addEventListener('keyup', reflectNumber);
+
+function reflectNumber(){
+numberCreditCard.textContent = inputNumberTC.value;
+}
+
+inputNumberTC.addEventListener('blur', returnReflectNumber);
+
+function returnReflectNumber(){
+  if (inputNumberTC.value == ''){
+  numberCreditCard.textContent = '#### #### #### ####';
+  }
+};*/
+
+//Reflejar Número en TC (Una sola función + switch para los eventos ¿If adentro?)
+inputNumberTC.addEventListener('keyup', reflectNumber);
+inputNumberTC.addEventListener('blur', reflectNumber);
+
+function reflectNumber(e){
+  switch (e.type) {
+    case 'keyup':
+      numberCreditCard.textContent = inputNumberTC.value;
+      break;
+    case 'blur':
+      if (inputNumberTC.value == ''){
+          numberCreditCard.textContent = "#### #### #### ####";}
+      break;}}
+
+//Reflejar Mes en TC
+inputMonthTC.addEventListener('change', (e) => {
+  monthCreditCard.textContent = e.target.value;});
+
+//Reflejar año en TC
+inputYearTC.addEventListener('change', (e) => {
+  yearCreditCard.textContent = e.target.value.slice(2);});
--- validator.js
+++ validator.js
@@ -1,21 +1,51 @@
-const validator = {
-  isvalid: function(creditCardNumber){
-    
-    //return =
-  }
+//Obtener el input de la tdc
+const inputNumberCard = document.getElementById('inputNumberCard');
 
+const validator = {
+  isvalid: function(cardNumber){
+    //Convertir el numero de la tdc en array y obtener el inverso
+    let algorithmArray = cardNumber.split('').reverse();
+
+//----------------------------- forEach ----------------------------------------------
+    algorithmArray.forEach((element, index) => {
+
+    //Obtener numeros en las posiciones par y multiplicarlos por 2
+     if (index & 1) {
+      element = element * 2
+
+      //A numeros mayores o iguales a 10, convertirlos en string y separar los dos digitos con split
+      if (element >= 10) {
+        element = element.toString().split('');
+
+        //Convertir los digitos en numeros y sumarlos entre si
+        element = parseInt(element[0]) + parseInt(element[1]);
+      }
+    }
+
+    //Colocar lo numeros que quedan en cada posicion del array
+    algorithmArray[index] = parseInt(element);
+
+    //Sumar todos los elementos del array
+    let sum = algorithmArray.reduce(function(x,y){
+      return x+y;
+    });
+
+    //Declarar el residuo de la division entre 10
+    let residue = sum % 10;
+
+    //Si el residuo es igual a 0 la tarjeta es valida, sino, es invalida
+    if (residue = 0) {
+      return true;
+    } else {
+      return false;
+    }
+    });
+  }
 };
-//Obtener el numero de la tdc ingresado en el input y almacenar en una variable
-let creditCardNumber = document.getElementById('card');
-//Obtener el inverso del numero de la tarjeta
-// function invert(creditCardNumber){
-//   let i = creditCardNumber.length;
-//   let creditCarNumberInvest = "";
-//   while (i>=0){
-//     creditCarNumberInvest = creditCarNumberInvest + creditCardNumber.charAt(i);
-//     i--;
-//   }
-//   return creditCarNumberInvest;
-// }
+
+//----------------------- Codigo para pruebas ----------------------------------------
+// inputNumberCard.addEventListener('blur', function(){
+//   validator.isvalid(inputNumberCard.value)
+// });
 
 export default validator;
